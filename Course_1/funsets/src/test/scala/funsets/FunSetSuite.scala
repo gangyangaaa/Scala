d1d3455e2c1df54afc14c7140d94fe33dcf83a7c
Yang Gang <34210644+gangyangaaa@users.noreply.github.com>
2022-03-13 17:59:28 -0500

    funsets implement

--- Course_1/funsets/src/test/scala/funsets/FunSetSuite.scala
+++ Course_1/funsets/src/test/scala/funsets/FunSetSuite.scala
@@ -44,7 +44,7 @@ class FunSetSuite extends munit.FunSuite:
    * Once you finish your implementation of "singletonSet", remove the
    * .ignore annotation.
    */
-  test("singleton set one contains one".ignore) {
+  test("singleton set one contains one") {
 
     /**
      * We create a new instance of the "TestSets" trait, this gives us access
@@ -66,6 +66,52 @@ class FunSetSuite extends munit.FunSuite:
       assert(!contains(s, 3), "Union 3")
   }
 
+  test("intersect contains all same elements") {
+    new TestSets:
+      val sa = union(s1, s2)
+      val sb = union(s1, s3)
+      val sab = intersect(sa, sb)
+      assert(contains(sab, 1), "Intersect 1")
+      assert(!contains(sab, 2), "Intersect not 2")
+  }
+
+  test("diff test") {
+    new TestSets:
+      val sd_1 = union(s1, s2)
+      val sd_2 = union(s1, s3)
+      val sd_diff = diff(sd_1, sd_2)
+      assert(contains(sd_diff, 2), "diff 2")
+  }
+
+  test("filter test") {
+    new TestSets:
+      val sa = union(s1, s2)
+      val s = filter(sa, s2)
+      assert(contains(s, 2), "filter 2")
+  }
+
+  test("for all") {
+    new TestSets:
+      val s = union(s1, s3)
+      val temp = union(s1, s2)
+      val f = union(s, temp)
+      assert(forall(s, f), "forall")
+  }
+
+  test("exist") {
+    new TestSets:
+      val s = union(s1, s2)
+      assert(exists(s1, s))
+      assert(!exists(s3, s))
+  }
+
+  test("map") {
+    new TestSets:
+      val s = union(s1, s2)
+      val f = map(s, x => x * x)
+      assert(contains(f, 4))
+      assert(contains(f, 1))
+  }
 
 
   import scala.concurrent.duration.*
